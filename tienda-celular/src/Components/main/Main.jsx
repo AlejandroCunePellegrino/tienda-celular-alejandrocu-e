4377720597a9ed26ef27f347bd59c72119a9620b
Alejandro Cuñe <alejandrocune97@gmail.com>
2022-06-26 21:04:08 -0300

    correcciones

--- tienda-celular/src/Components/main/Main.jsx
+++ tienda-celular/src/Components/main/Main.jsx
@@ -12,9 +12,7 @@ const Main = () => {
         <main class= "container-fluid">
             <div class= "row justify-content-center" style={styles.section} id="samnsung">
                 <h2 class="text-center">Samnsung</h2>
-                <Cards name='Samnsung Galaxy A20' price='$ 50.990'  image= {a20} >
-                    <ItemCount stock= {10} initial= {1} />
-                </Cards>  
+                <Cards name='Samnsung Galaxy A20' price='$ 50.990'  image= {a20} stock= {10} initial= {1} />
                 <Cards name='Samnsung Galaxy A12' price='$ 44.999' image= {a12} stock= {12} initial= {1}/> 
                 <Cards name='Samnsung Galaxy S22 Ultra' price='$ 262.999' image= {s22} stock= {5} initial= {1} /> 
                 <Cards name='Samnsung Galaxy S20 FE' price='$ 91.999' image= {s20} stock= {8}  initial= {1} /> 
