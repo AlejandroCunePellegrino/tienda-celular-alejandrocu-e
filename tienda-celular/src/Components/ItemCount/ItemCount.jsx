98b3134969940b767d86ee72e79d32b71ba54d56
Alejandro Cuñe <alejandrocune97@gmail.com>
2022-07-14 22:47:03 -0300

    sincronizar counter

--- tienda-celular/src/Components/ItemCount/ItemCount.jsx
+++ tienda-celular/src/Components/ItemCount/ItemCount.jsx
@@ -1,8 +1,8 @@
 import React, {useState} from "react";
-import Swal from 'sweetalert2'
+
 import addCart from '../../assets/images/addCart.png'
 
-const ItemCount = ({stock, initial, name}) => {
+const ItemCount = ({stock, initial, onAdd}) => {
 
     const [amount, setAmount] = useState(1)
     const count = (value) => { 
@@ -11,14 +11,11 @@ const ItemCount = ({stock, initial, name}) => {
             setAmount(amount+value) 
         } }
 
-        const addToCart = () =>{
-            Swal.fire(`Se agregaron ${amount} ${name} al carrito`)
-        }
 
      return(
         <>
         <div className="containerCart">
-                <span onClick={()=>addToCart()}><img src={addCart} alt="addcart"  className="cartImg"/></span>
+                <span onClick={() => onAdd(amount)}><img src={addCart} alt="addcart"  className="cartImg"/></span>
             </div>
         <div className="d-flex justify-content-between containerBtn">
             <span onClick={()=>count(-1)} className= "btn btnResta">-</span>
--- tienda-celular/src/Components/ItemDetail/ItemDetail.jsx
+++ tienda-celular/src/Components/ItemDetail/ItemDetail.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import ItemDetailcss from './ItemDetail.css'
 import ItemCount from '../ItemCount/ItemCount'
+import Swal from 'sweetalert2'
+import { Link } from "react-router-dom";
 
-const ItemDetail = ({image, detail, stock, name, initial}) => {
+const ItemDetail = ({image, detail, stock, name, initial, price}) => {
+
+
+    const [gotocart, setGoToCart] = useState(false)
+    const onAdd = (quantity) =>{
+        Swal.fire(`Se agregaron ${quantity} ${name} al carrito`);
+        setGoToCart(true)  
+    }
+
+
     return(
         <div className="box">
             <div className="box-img">
-                <img src={image} alt={name} /* className="img-a12" */ />
+                <img src={image} alt={name} />
             </div>
             <div className="box-detail">
                 <h5>{name}</h5>
+                <h6>{price}</h6>
                 <p>{detail}</p>
-                <ItemCount stock={stock} initial={initial} />
+                {
+                    gotocart
+                    ? <Link to='/cart'>Terminar compra</Link>
+                    :<ItemCount stock={stock} initial={initial} name={name} onAdd={onAdd} />
+                }
             </div>
         </div>
     )
