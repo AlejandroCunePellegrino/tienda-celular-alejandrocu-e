b99ea6ab6ba850f39bfda07ebfedb39b92303404
Alejandro Cuñe <alejandrocune97@gmail.com>
2022-07-21 12:00:36 -0300

    se termino addProduct, falta cartview

--- tienda-celular/src/Components/CartContext/CartContext.jsx
+++ tienda-celular/src/Components/CartContext/CartContext.jsx
@@ -1,5 +1,5 @@
-import React, {createContext, useState, useContext} from "react";
-/* import { useContext } from "react"; */
+import React, {createContext, useState, useContext, useEffect} from "react";
+
 
 export const context = createContext();
 const {Provider} = context
@@ -8,22 +8,30 @@ export const useCartContext = () => useContext(context)
 export const CartContext = ({children}) => {
 
     const [cart, setCart] = useState([]);
-
+    
+    useEffect(() => {
+        console.log(cart);
+    },[cart])
+    
     const addProduct = (item, newQuantity) => {
         const newCart = cart.filter(prod => prod.id !== item.id);
         newCart.push({ ...item, quantity: newQuantity});
         setCart(newCart)
-        console.log(newCart);
     }
 
-    const clearCart = () => setCart([]);
-    
+    const clearCart = () => {
+        setCart([])
+        
+    };
+        
     const isInCart = (id) => {
         return cart.find(product => product.id === id) ? true : false;
     }
 
-    const removeProduct = (id) => setCart(cart.filter(product => product.id !== id));
-    
+    const removeProduct = (id) => {
+        setCart(cart.filter(product => product.id !== id))
+        
+    };
     return(
         <Provider value={{clearCart, isInCart, removeProduct, addProduct}}>
             {children}
